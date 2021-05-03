1fc806560eb55a1f845f4a7add17099e1450505b
Demna Saghliani <saghliani.d@gmail.com>
2021-05-03 22:28:36 +0400

    Recreate the "local screen" solution, but update `VideoController` to search for screens via `[custom-screen]` and receive commands to change the URL (?). Change screen audio settings upon creation (in `hub.js`).

--- src/utils/video-controller.js
+++ src/utils/video-controller.js
@@ -1,10 +1,7 @@
 // INTERVAL_DURATION: The time, in seconds, for the client to send a 'reminder' message to the WebSocket
 // server, in the name of keeping the connection alive. Disconnection seems to happen in ~600 seconds.
-const INTERVAL_DURATION = 500;
+const INTERVAL_DURATION = 30;
 const WEB_SOCKET_URL = 'wss://qceh63bc59.execute-api.eu-west-1.amazonaws.com/production';
-// MAIN_SCREEN_ID: Eventually, when multiple screens are present, screen ID can be incorporated into the
-// WebSocket-delivered commands. Until then, it's simpler to keep the sole ID in a constant.
-const MAIN_SCREEN_ID = 'naf-20BD6EAF-D619-44EE-8222-5A2019D21AC3';
 
 export class VideoController {
     constructor() {
@@ -17,6 +14,7 @@ export class VideoController {
         
         this.socket.onclose = _e => {
             console.log('Disconnected from the WebSocket');
+            clearInterval(this.checkInInterval);
         };
         
         this.socket.onmessage = e => {
@@ -30,17 +28,17 @@ export class VideoController {
             }
 
             const command = data.command;
-            const screen = this.findScreen(MAIN_SCREEN_ID);
+            console.log(command);
             
-            switch (command) {
-                case 'enable':
-                    this.setVideoStatus(screen, true);
-                    break;
-                case 'disable':
-                    this.setVideoStatus(screen, false);
-                    break;
-                default:
-                    console.log('Received an unknown command:', command);
+            for (const key in command) {
+                switch(key) {
+                    case 'newUrl':
+                        this.setVideoUrl(command[key]);
+                        break;
+                    case 'newStatus':
+                        this.setVideoStatus(command[key]);
+                        break;
+                }
             }
         };
     }
@@ -48,21 +46,26 @@ export class VideoController {
     disconnect() {
         console.log("Manually disconnecting from the WebSocket.");
         this.socket.close(1000);
-        clearInterval(this.checkInInterval);
     }
 
-    setVideoStatus(screen, enabled) {
+    setVideoUrl(newUrl) {
+        const screen = this.getScreen();
+        screen.setAttribute("media-loader", "src", newUrl);
+    }
+
+    setVideoStatus(enabled) {
+        const screen = this.getScreen();
         screen.setAttribute('video-pause-state', 'paused', enabled ? false : true);
         screen.setAttribute('visible', enabled ? true : false);
-    } 
+    }
     
     sendReminder() {
         const payload = { 'action': 'remind' };
         this.socket.send(JSON.stringify(payload));
-        console.log('Sent a check-in message:', payload);
+        console.log('Sent a check-in message.');
     }
 
-    findScreen(screenId) { 
-        return document.querySelector(`#${screenId}`);
+    getScreen() { 
+        return document.querySelector('[custom-screen]');
     }
 }
